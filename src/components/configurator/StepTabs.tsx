a06c20ac1d710e3c9a1551c05253b979a29f5948
vva45 <vintila.vlad.vv@gmail.com>
2026-05-28 11:09:42 +0200

    website vercel 1.1

--- src/components/configurator/StepTabs.tsx
+++ src/components/configurator/StepTabs.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react"
 import ConfigStepProfile from "./ConfigStepProfile"
 
 export default function StepTabs() {
+  const [value, setValue] = useState<any>(null)
+
+  const handleChange = (nextValue: any) => {
+    setValue(nextValue)
+  }
 
   return (
 
     <div className="rounded-[28px] bg-[#071120] border border-white/5 p-8">
 
-      <ConfigStepProfile />
+      <ConfigStepProfile value={value} onChange={handleChange} />
 
     </div>
 
